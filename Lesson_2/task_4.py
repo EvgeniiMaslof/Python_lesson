b43edbfa9f1f41ce1c34b4e74cdcd80dd70f7a44
evgenii <evgenii_08@list.ru>
2022-10-01 01:59:40 +0300

    changing the solution of problem four

--- Lesson_2/task_4.py
+++ Lesson_2/task_4.py
@@ -3,8 +3,7 @@ Position_two = int(input("Введите второе число:" ))
 n = int(input('Введите N= '))
 num = range(-n, n+1)
 numbers = list(num)
-for i in numbers:
-    print(i)
+print(numbers)
 if Position_one <= len(numbers) >= Position_two:
      composition = numbers[Position_one - 1] * numbers[Position_two - 1]
      print(f"Произведение позиций {composition}")

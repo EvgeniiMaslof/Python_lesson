2f89af61a67955100100548225bd44c9cc380b98
evgenii <evgenii_08@list.ru>
2022-10-01 01:27:13 +0300

    changing the solution of problem four

--- Lesson_2/task_4.py
+++ Lesson_2/task_4.py
@@ -1,10 +1,12 @@
+Position_one = int(input("Введите первое число:" ))
+Position_two = int(input("Введите второе число:" ))
 n = int(input('Введите N= '))
 num = range(-n, n+1)
 numbers = list(num)
 for i in numbers:
     print(i)
-Position_one = int(input("Введите первое число:" ))
-Position_two = int(input("Введите второе число:" ))
-composition = numbers[Position_one - 1] * numbers[Position_two - 1]
-print(composition)
-
+if Position_one <= len(numbers) >= Position_two:
+     composition = numbers[Position_one - 1] * numbers[Position_two - 1]
+     print(composition)
+else:
+    print("Число вне диапазона списка!")

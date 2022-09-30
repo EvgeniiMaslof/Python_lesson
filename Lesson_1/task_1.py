53c9a36225a7dde76a0d620937c60f576666bce6
evgenii <evgenii_08@list.ru>
2022-09-30 20:17:13 +0300

    changing_the_decision_of_the_first_building

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -1,10 +1,8 @@
-while True:
-    user_week = int(input("Введите порядковый номер дня недели от 1 до 7: "))
-    if 1 <= user_week <= 7:
-        break
-
-days_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" ]
-
-if 6 <= user_week <= 7:
-    print(f"{days_week[user_week - 1]}, выходной.")
-else: print(f"{days_week[user_week - 1]}, рабочий день.")
+numbers_week = int(input("Введите номер дня недели от 1 до 7: "))
+days_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+if 1 <= numbers_week <= 5:
+    print(f"{days_week[numbers_week - 1]}, рабочий день.")
+elif 6 <= numbers_week <= 7:
+    print(f"{days_week[numbers_week - 1]}, выходной.")
+else:
+    print("Неправилное число, попробуйте снова.")

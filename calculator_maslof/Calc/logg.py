ff15bd4e265388a755b10a6cf28526d587f9ab1b
Evgenii Maslof <110404284+EvgeniiMaslof@users.noreply.github.com>
2022-10-26 22:08:31 +0300

    delet

--- calculator_maslof/Calc/logg.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime as dt
-
-
-def input_logger(data):
-    time = dt.now().strftime('%D  %H:%M')
-    with open('logg.csv', 'a', encoding='utf-8') as file:
-        file.write(f'{data}, {time} \n')
